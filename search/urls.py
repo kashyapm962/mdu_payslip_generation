bad2dd7227b977bd0dd8d337f369195eb29efb42
Manish <kashyapm962@gmail.com>
2018-09-12 01:08:49 +0530

    Added a new form

--- search/urls.py
+++ search/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from . import views
+app_name = 'search'
 
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^$', views.index, name ='index'),
+    url(r'^result/$' , views.result , name='result'),    
 ]
 
 
--- search/views.py
+++ search/views.py
@@ -1,6 +1,15 @@
 from django.http import HttpResponse
-
+from django.shortcuts import get_object_or_404, render
+from django.template import loader
+from django import forms  
 # Create your views here.
 
-def index(Request):
-	return HttpResponse('<h1> This is working just fine </h1>')
+def index(request):
+	#template = loader.get_template('search/index.html')
+	return render(request,'search/index.html')
+	
+	
+	
+def result(request):
+	form = forms.ContactForm()
+	return HttpResponse("<h1>This is working :"+ str(form)+"</h1>",year)
